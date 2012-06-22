a970bdb9dcb157cb93adc4b72a9a3e03f0ab2cca
Gabor Szabo <szabgab@gmail.com>
2012-06-22 20:12:03 +0300

    start the Perl 6 Pod::Parser

---
+++ lib/Pod/Parser.pm
@@ -0,0 +1,13 @@
+use v6;
+module Pod::Parser;
+
+
+=begin pod
+
+=head1 NAME
+
+Pod::Parser - parsing files with POD in them (Perl 6 syntax)
+
+=end pod
+
+# vim: ft=perl6
---
+++ t/01-parse.t
@@ -0,0 +1,8 @@
+use v6;
+use Test;
+plan 1;
+
+use Pod::Parser;
+ok 1, 'Loading module succeeded';
+
+done;
