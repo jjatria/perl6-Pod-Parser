416658440b11932d405aba0025d669f068629ea9
Gabor Szabo <szabgab@gmail.com>
2012-06-23 10:46:25 +0300

    parse (badly) first example file

--- lib/Pod/Parser.pm
+++ lib/Pod/Parser.pm
@@ -1,7 +1,6 @@
 use v6;
 class Pod::Parser;
 
-
 =begin pod
 
 =head1 NAME
@@ -9,5 +8,44 @@ class Pod::Parser;
 Pod::Parser - parsing files with POD in them (Perl 6 syntax)
 
 =end pod
+
+my $in_pod = 0;
+my $pod = '';
+my $text = '';
+
+method parse (Str $string) {
+	my @lines = $string.split("\n");
+	my @data;
+	for @lines -> $row {
+		if $row ~~ m/^\=begin \s+ pod \s* $/ {
+			$in_pod = 1;
+			if $text ne '' {
+				@data.push($text);
+				$text = '';
+			}
+			next;
+		}
+		if $row ~~ m/^\=end \s+ pod \s* $/ {
+			$in_pod = 0;
+			if $pod ne '' {
+				@data.push($pod);
+				$pod = '';
+			}
+			next;
+		}
+		if $in_pod {
+			$pod ~= "$row\n";
+			next;
+		}
+		$text ~= "$row\n";
+	}
+	return @data;
+}
+
+method parse_file (Str $filename) {
+	my $string = slurp($filename);
+	self.parse($string);
+}
+
 
 # vim: ft=perl6
--- t/01-parse.t
+++ t/01-parse.t
@@ -1,11 +1,22 @@
 use v6;
 use Test;
-plan 2;
+plan 3;
 
 use Pod::Parser;
 ok 1, 'Loading module succeeded';
 
 my $pp = Pod::Parser.new;
 isa_ok $pp, 'Pod::Parser';
+
+my @result = $pp.parse_file('t/files/a.pod');
+is_deeply @result, Array.new(
+	"text before\n\n",
+	"\n=head1 NAME\n\nText in name\n\n=head1 SYNOPSIS\n\n    some verbatime\n    text\n\n"), 'parse a.pod';
+#say Pod::Parser::parse($case);
+
+#say %tree;
+
+#is_deeply(
+
 
 done;
